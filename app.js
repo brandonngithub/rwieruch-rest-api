b450f4e5cdd7f43c2d5900f064ee954c44b06b7a
Brandon Ngo <brandonngo56@gmail.com>
2025-03-12 14:50:18 -0400

    Application-level Express middleware

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 const PORT = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  req.me = users[1];
+  next();
+});
 
 let users = {
   1: {
@@ -57,6 +66,34 @@ app.get('/messages', (req, res) => {
 
 app.get('/messages/:messageId', (req, res) => {
   return res.send(messages[req.params.messageId]);
+});
+
+app.post('/messages', (req, res) => {
+  const id = uuidv4();
+  const message = {
+    id,
+    text: req.body.text,
+    userId: req.me.id,
+  };
+
+  messages[id] = message;
+
+  return res.send(message);
+});
+
+app.delete('/messages/:messageId', (req, res) => {
+  const {
+    [req.params.messageId]: message,
+    ...otherMessages
+  } = messages;
+
+  messages = otherMessages;
+
+  return res.send(message);
+});
+
+app.get('/session', (req, res) => {
+  return res.send(users[req.me.id]);
 });
 
 app.listen(PORT, () => {
