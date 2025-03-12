21110189df59351b1ab1f259065451dd1ba03586
Brandon Ngo <brandonngo56@gmail.com>
2025-03-12 14:34:23 -0400

    HTTP methods are REST operations

--- app.js
+++ app.js
@@ -4,7 +4,19 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  return res.send('Received a GET HTTP method');
+});
+
+app.post('/', (req, res) => {
+  return res.send('Received a POST HTTP method');
+});
+
+app.put('/', (req, res) => {
+  return res.send('Received a PUT HTTP method');
+});
+
+app.delete('/', (req, res) => {
+  return res.send('Received a DELETE HTTP method');
 });
 
 app.listen(PORT, () => {
