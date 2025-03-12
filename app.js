441d009ce370c3bcf793c00249e0bb7eb3524119
Brandon Ngo <brandonngo56@gmail.com>
2025-03-12 14:41:34 -0400

    Making sense of REST with Express

--- app.js
+++ app.js
@@ -3,8 +3,36 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+let users = {
+  1: {
+    id: '1',
+    username: 'Robin Wieruch',
+  },
+  2: {
+    id: '2',
+    username: 'Dave Davids',
+  },
+};
+
+let messages = {
+  1: {
+    id: '1',
+    text: 'Hello World',
+    userId: '1',
+  },
+  2: {
+    id: '2',
+    text: 'By World',
+    userId: '2',
+  },
+};
+
 app.get('/users', (req, res) => {
-  return res.send('GET HTTP method on user resource');
+  return res.send(Object.values(users));
+});
+
+app.get('/users/:userId', (req, res) => {
+  return res.send(users[req.params.userId]);
 });
 
 app.post('/users', (req, res) => {
@@ -21,6 +49,14 @@ app.delete('/users/:userId', (req, res) => {
   return res.send(
     `DELETE HTTP method on user/${req.params.userId} resource`,
   );
+});
+
+app.get('/messages', (req, res) => {
+  return res.send(Object.values(messages));
+});
+
+app.get('/messages/:messageId', (req, res) => {
+  return res.send(messages[req.params.messageId]);
 });
 
 app.listen(PORT, () => {
